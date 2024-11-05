20d37ca3e22a7b57239bc9b34a1527699e009330
mmu101 <lotuseaters101@gmail.com>
2024-11-05 16:13:40 +0100

    Multiplication Table of a Number
    Multiplication Table of a Number

---
+++ Basic_CPP_Programs/MultiplicationOfATable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+	int num=0;
+	std::cout<<"Enter the number\n";
+	std::cin>>num;
+	for(int i=1;i<=10;i++)
+		std::cout<<"num * i = "<<num*i<<"\n";
+	return 0;
+}
+  
